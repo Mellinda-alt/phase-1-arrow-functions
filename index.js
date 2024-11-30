b01e10182f31fc439b6472ddc69e575c80dd5814
Mellinda-alt <mellindawambui@gmail.com>
2024-11-30 18:21:04 +0300

    modified index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = (a,b) => a / b
+
+//console.log(divide(2000/100));
+
+// Output:20
+
+const square = (b) => b * b
+
+//console.log(square(9));
+
+//Output:81
+
+
+const add = (a,b) => a + b
+
+//console.log(add(8,4));
+
+//Output:12
+
+
